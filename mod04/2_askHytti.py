8d17827e8c12832e5191b128848c08bbf3d9eeac
petrket <petri.k.kettunen@metropolia.fi>
2026-08-17 15:26:19 +0300

    askHytti kesken

---
+++ mod04/2_askHytti.py
@@ -0,0 +1,12 @@
+hytti = input("Mikä hyttiluokka?\n")
+
+if hytti == "LUX" or "lux" or "Lux":
+    print ("Parvekkeellinen hytti yläkannella")
+elif hytti == "A" or "a":
+    print ("Ikkunallinen hytti autokannen yläpuolella")
+elif hytti == "B" or "b":
+    print ("ikkunaton hytti autokannen yläpuolella")
+elif hytti == "C" or "c":
+    print ("ikkunaton hytti autokannen yläpuolella")
+else:
+    print("Virheellinen hyttiluokka") 
