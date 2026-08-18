7e725f28b2a7950fe41ceb44a86df0c65715f5c5
pjootrz <kettunen.petri@gmail.com>
2026-08-18 20:18:17 +0300

    mod06-03 done

---
+++ mod06/3_checkPrimeNumber.py
@@ -0,0 +1,22 @@
+#https://stackoverflow.com/questions/10712002/create-an-empty-list-with-certain-size-in-python
+
+numberInput = int(input("Check if number is a prime number\n"))
+
+numbers = list(range(numberInput))
+## TAI NÄIN
+# for i in range(numberInput):
+#     numbers.append(i)
+
+primeNumber = bool
+
+#aloitetaan indexistä 2, koska emme halua jakaa 0:lla tai 1:llä
+for n in range(2, numberInput):
+    if numberInput % numbers[n] == 0:
+        primeNumber = False
+        print("Not a prime number")
+        break
+    elif numberInput % numbers[n] != 0:
+        primeNumber = True
+
+if primeNumber == True:
+    print("It's a prime number!")
