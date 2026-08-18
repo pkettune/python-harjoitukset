e17636b9a107c6438eb91678332e1eb64bb8929b
pjootrz <kettunen.petri@gmail.com>
2026-08-18 17:10:20 +0300

    askDices fixed

--- mod06/1_askDices.py
+++ mod06/1_askDices.py
@@ -4,10 +4,16 @@ askedDice = int(input("How many dices you want to roll? "))
 
 dices = []
 
+sum = 0
+
 i = 0
 while i < askedDice:
-    dices.append(askedDice)
+    dice = random.randint(1, 6)
+    dices.append(dice)
     i = i + 1
 
-for i in dices:
-    print (random.randint(1, 6))
+for d in dices:
+    sum = sum + d
+    print (dices)
+
+print (sum)
