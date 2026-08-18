a24a79dd8e8379e7da7e3b69547af4efc164522d
petrket <petri.k.kettunen@metropolia.fi>
2026-08-18 14:14:12 +0300

    mod05 peliprojekti kesken

--- peliprojekti/nameGame.py
+++ peliprojekti/nameGame.py
@@ -1,5 +1,29 @@
-name = input("WHAT IS YOUR NAME?\n")
-age = input("WHAT IS YOUR AGE?\n")
+import random
 
-print (f"Player name: {name}\nPlayer age: {age}")
+name = input("WHAT IS YOUR NAME?\n")
+age = int(input("WHAT IS YOUR AGE?\n"))
+komento = str
+
+while(input):
+    if(age < 12):
+        print("alaikäinen")
+        break
+    if(age >= 12):
+        print("\nwelcome " + name+"\n")
+
+    while(komento != "lopeta"):
+        print("Komennot:\n'a'\n'dice'\n'c'\n'lopeta'\n")
+        komento = input("Anna komento: ")
+        if komento == "a":
+            print("a")
+            break
+        if komento == "dice":
+            print(f"\nDice rolled: {random.randint(1, 6)}")
+            break
+        if komento == "c":
+            print("c")
+            break
+    
+
+#print (f"Player name: {name}\nPlayer age: {age}")
 
