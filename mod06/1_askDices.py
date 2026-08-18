1bfa900eb30458548aed3cbb903bd024bda0518c
pjootrz <kettunen.petri@gmail.com>
2026-08-18 17:22:00 +0300

    askDices the other way

--- mod06/1_askDices.py
+++ mod06/1_askDices.py
@@ -1,3 +1,5 @@
+#ORIGINAL---------------------------------
+
 import random
 
 askedDice = int(input("How many dices you want to roll? "))
@@ -17,3 +19,18 @@ for d in dices:
     print (dices)
 
 print (sum)
+
+
+# #FINETUNED--------------------------------
+
+# import random
+
+# askedDice = int(input("How many dices you want to roll? "))
+# dices = [0 for _ in range(askedDice)]
+# #https://stackoverflow.com/questions/10712002/create-an-empty-list-with-certain-size-in-python
+# #you can do this: a = [0 for _ in range(10)]
+
+# sum = 0
+# for d in dices:
+#     sum = sum + random.randint(1, 6)
+# print (sum)
