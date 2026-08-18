de28dcfd3469cce9a530b2fd742dfd34b4677de8
petrket <petri.k.kettunen@metropolia.fi>
2026-08-18 14:45:56 +0300

    mod05 peliprojekti valmis

--- peliprojekti/nameGame.py
+++ peliprojekti/nameGame.py
@@ -4,26 +4,23 @@ name = input("WHAT IS YOUR NAME?\n")
 age = int(input("WHAT IS YOUR AGE?\n"))
 komento = str
 
-while(input):
+while(komento != "lopeta"):
     if(age < 12):
         print("alaikäinen")
         break
     if(age >= 12):
-        print("\nwelcome " + name+"\n")
+        print("\nwelcome " + name)
 
-    while(komento != "lopeta"):
-        print("Komennot:\n'a'\n'dice'\n'c'\n'lopeta'\n")
-        komento = input("Anna komento: ")
-        if komento == "a":
-            print("a")
-            break
-        if komento == "dice":
-            print(f"\nDice rolled: {random.randint(1, 6)}")
-            break
-        if komento == "c":
-            print("c")
-            break
-    
-
-#print (f"Player name: {name}\nPlayer age: {age}")
-
+        while (input):
+            print("\nKomennot:\n'a'\n'dice'\n'time'\n'lopeta'\n")
+            komento = input("Anna komento: ")
+            if komento == "a":
+                print("\na is the first letter of the alphabet")
+            elif komento == "dice":
+                print(f"\nDice rolled: {random.randint(1, 6)}")
+            elif komento == "time":
+                print("\nit's showtime")
+            elif (komento == "lopeta"):
+                break
+            else:
+                print("\nWRONG INPUT!")
