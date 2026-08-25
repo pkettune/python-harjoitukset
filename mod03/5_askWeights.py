b7d14b878b826129cf122684c7b82aa7bc950132
petrket <petri.k.kettunen@metropolia.fi>
2026-08-25 13:49:04 +0300

    asdf

--- mod03/5_askWeights.py
+++ mod03/5_askWeights.py
@@ -4,13 +4,14 @@ leiviskat = float(input("Anna leiviskät.\n"))
 naulat = float(input("Anna naulat.\n"))
 luodit = float(input("Anna luodit.\n"))
 
-luotiPaino = float(13.3)
-naulaPaino = float(32 * luotiPaino)
-leiviskaPaino = float(20 * naulaPaino)
+luotiPaino = 13.3
+naulaPaino = 32 * luotiPaino
+leiviskaPaino = 20 * naulaPaino
 
 kokonaispaino = luotiPaino * luodit + naulaPaino * naulat + leiviskaPaino * leiviskat
 kilot = int(kokonaispaino / 1000)
-grammat = float (kokonaispaino % 1000)
+# TAI kilot = kokonaispaino // 1000
+grammat = kokonaispaino % 1000
 print (f"Massa nykymittojen mukaan:\n{kilot} kilogrammaa ja {grammat:3.2f} grammaa.")
 
 
