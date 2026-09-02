89af39969e6e4f6a1311c240a48064000064f03e
pjootrz <kettunen.petri@gmail.com>
2026-09-02 09:47:18 +0300

    asdf

--- mod05/6_pi.py
+++ mod05/6_pi.py
@@ -1,19 +1,20 @@
 import random
 
 numberOfPoints = int(input("how many points"))
-i = 0
+
 N = numberOfPoints
 n = 0 # number of points inside circle
 
 min = -1
 max = 1
 
+i = 0
 while (i <= numberOfPoints):
     x = random.uniform(min, max)
     y = random.uniform(min, max)
     if (x**2+y**2 < 1):
-        n = n + 1
-    i = i + 1
+        n += 1
+    i += 1
 
 print(f"{4*n/N}")
 
