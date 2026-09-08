82737492c8109a916e19ecbe107b3740d07b3ae3
pjootrz <kettunen.petri@gmail.com>
2026-09-08 14:58:23 +0300

    finetune dice

--- mod07/2_throwRandomDiceUntilMax.py
+++ mod07/2_throwRandomDiceUntilMax.py
@@ -5,7 +5,6 @@ dice = 0
 sideInput = int(input("How many sides in the die? "))
 
 def throw(sideCount):
-    sideCount = sideInput
     return random.randint(1,sideCount)
 
 while (dice != sideInput):
